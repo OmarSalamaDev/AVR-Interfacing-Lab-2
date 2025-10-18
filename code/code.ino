3a2ee706a374619aad455874843e82605adbd3dc
Omar Salama <omar550205@gmail.com>
2025-10-18 15:30:39 +0300

    7-segment implemented

--- code/code.ino
+++ code/code.ino
@@ -31,6 +31,9 @@ bool status = 0;  //!~ 0=off, 1=on
 bool dir = 1;     //!~ 0=reverse, 1=forward
 
 
+void display_mode(char ch);
+
+
 void setup() {
   //?~ btns setup 
   pinMode(MODE_BTN, INPUT_PULLUP);
@@ -45,6 +48,7 @@ void setup() {
   for(uint8_t i = 0; i < 7; i++) pinMode(SEG_7[i], OUTPUT);
 }
 
+
 void loop() {
 
   if(!digitalRead(START_STOP_BTN)) status = !status;
@@ -53,10 +57,82 @@ void loop() {
     if(!digitalRead(DIR_BTN)) dir = !dir;
     digitalWrite(STATUS_LED, 1);
     digitalWrite(DIR_LED, dir);
+    display_mode('0');
+    delay(1000);
+    display_mode('1');
+    delay(1000);
+    display_mode('2');
+    delay(1000);
+    display_mode('3');
+    delay(1000);
+    display_mode('4');
+    delay(1000);  
+    display_mode('E');
+    delay(1000);
   }
   else {
     digitalWrite(STATUS_LED, 0);
     digitalWrite(DIR_LED, 0);
   }
 
+}
+
+
+void display_mode(char ch)	{
+  switch(ch) {
+    case '0':
+      digitalWrite(SEG_7[0], 1);	
+      digitalWrite(SEG_7[1], 1);	
+      digitalWrite(SEG_7[2], 1);
+      digitalWrite(SEG_7[3], 1);
+      digitalWrite(SEG_7[4], 1);
+      digitalWrite(SEG_7[5], 1);
+      digitalWrite(SEG_7[6], 0); 
+    break;
+    case '1':
+      digitalWrite(SEG_7[0], 0);	
+      digitalWrite(SEG_7[1], 1);
+      digitalWrite(SEG_7[2], 1);
+      digitalWrite(SEG_7[3], 0);	
+      digitalWrite(SEG_7[4], 0);
+      digitalWrite(SEG_7[5], 0);
+      digitalWrite(SEG_7[6], 0);
+    break;
+    case '2':
+      digitalWrite(SEG_7[0], 1);
+      digitalWrite(SEG_7[1], 1);
+      digitalWrite(SEG_7[2], 0);
+      digitalWrite(SEG_7[3], 1);
+      digitalWrite(SEG_7[4], 1);
+      digitalWrite(SEG_7[5], 0);
+      digitalWrite(SEG_7[6], 1);
+    break;
+    case '3':
+      digitalWrite(SEG_7[0], 1);
+      digitalWrite(SEG_7[1], 1);
+      digitalWrite(SEG_7[2], 1);
+      digitalWrite(SEG_7[3], 1);
+      digitalWrite(SEG_7[4], 0);
+      digitalWrite(SEG_7[5], 0);
+      digitalWrite(SEG_7[6], 1);
+    break;
+    case '4':
+      digitalWrite(SEG_7[0], 0);
+      digitalWrite(SEG_7[1], 1);
+      digitalWrite(SEG_7[2], 1);
+      digitalWrite(SEG_7[3], 0);
+      digitalWrite(SEG_7[4], 0);
+      digitalWrite(SEG_7[5], 1);
+      digitalWrite(SEG_7[6], 1);
+    break;
+    default:
+      digitalWrite(SEG_7[0], 0);
+      digitalWrite(SEG_7[1], 0);
+      digitalWrite(SEG_7[2], 0);
+      digitalWrite(SEG_7[3], 0);
+      digitalWrite(SEG_7[4], 0);
+      digitalWrite(SEG_7[5], 0);
+      digitalWrite(SEG_7[6], 0);
+    break;
+  }
 }
